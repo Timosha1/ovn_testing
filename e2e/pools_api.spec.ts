cbf1db29491cc214ca6b020bddd197370e560612
Timosha <johndoe@example.comtim.ilya.timofeev@gmail.com>
2025-03-07 14:41:55 +0400

    added expectedPoolAddress test, changed expected results

--- e2e/pools_api.spec.ts
+++ e2e/pools_api.spec.ts
@@ -33,6 +33,8 @@ interface PoolTestCase {
   expectedPoolName: string;
   expectedToken0Symbol: string;
   expectedToken1Symbol: string;
+  expectedPoolAddress: string;
+
 }
 
 const poolTestCases: PoolTestCase[] = [{
@@ -42,6 +44,7 @@ const poolTestCases: PoolTestCase[] = [{
   expectedPoolName: 'USDC/USD+',
   expectedToken0Symbol: 'USDC',
   expectedToken1Symbol: 'USD+',
+  expectedPoolAddress: '0x0c1A09d5D0445047DA3Ab4994262b22404288A3B',
 },
   {
     search: 'WETH/USD+',
@@ -50,6 +53,7 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'WETH/USD+',
     expectedToken0Symbol: 'WETH',
     expectedToken1Symbol: 'USD+',
+    expectedPoolAddress: '0x4D69971CCd4A636c403a3C1B00c85e99bB9B5606',
   },
   {
     search: 'USDC/USD+',
@@ -58,6 +62,7 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'USDC/USD+',
     expectedToken0Symbol: 'USDC',
     expectedToken1Symbol: 'USD+',
+    expectedPoolAddress: '0x167c9F0AF189DDF58f4B43683404a45096c23b67',
   },
   {
     search: 'WETH/XUSD',
@@ -66,6 +71,7 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'WETH/XUSD',
     expectedToken0Symbol: 'WETH',
     expectedToken1Symbol: 'XUSD',
+    expectedPoolAddress: '0x421803da50d3932caa36bd1731d36a0e2af93542',
   },
   {
     search: 'BSC-USD/WBNB',
@@ -74,14 +80,16 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'BSC-USD/WBNB',
     expectedToken0Symbol: 'BSC-USD',
     expectedToken1Symbol: 'WBNB',
+    expectedPoolAddress: '0x36696169C63e42cd08ce11f5deeBbCeBae652050',
   },
   {
     search: 'WETH/AERO',
     chainId: '8453',
-    platform: 'Aerodrome',
+    platform: 'Uniswap',
     expectedPoolName: 'WETH/AERO',
     expectedToken0Symbol: 'WETH',
     expectedToken1Symbol: 'AERO',
+    expectedPoolAddress: '0x3d5d143381916280ff91407febeb52f2b60f33cf',
   },
   {
     search: 'WETH/USDC',
@@ -90,6 +98,7 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'WETH/USDC',
     expectedToken0Symbol: 'WETH',
     expectedToken1Symbol: 'USDC',
+    expectedPoolAddress: '0x72AB388E2E2F6FaceF59E3C3FA2C4E29011c2D38',
   },
   {
     search: 'WETH/USDC',
@@ -98,6 +107,7 @@ const poolTestCases: PoolTestCase[] = [{
     expectedPoolName: 'WETH/USDC',
     expectedToken0Symbol: 'WETH',
     expectedToken1Symbol: 'USDC',
+    expectedPoolAddress: '0xd9e2a1a61B6E61b275cEc326465d417e52C1b95c',
   }
 ];
 
@@ -107,10 +117,9 @@ test.describe('Проверка API пулов для разных пар', () =
                                  chainId,
                                  platform,
                                  expectedPoolName,
-                                 expectedToken0Symbol,
-                                 expectedToken1Symbol
+                                 expectedPoolAddress
                                }) => {
-    test(`Пул ${expectedPoolName} есть в ответе API ${platform}`, async ({
+    test(`Pool ${expectedPoolName} ${platform} (${expectedPoolAddress}) found in the API response  (Chain ID: ${chainId})`, async ({
                                                                            request
                                                                          }) => {
       const queryParams = {
@@ -122,6 +131,7 @@ test.describe('Проверка API пулов для разных пар', () =
         limit: '30',
         sortByTvl: 'desc',
         score_gt: '20',
+        poolAddress: expectedPoolAddress,
       };
 
       const response = await request.get(baseUrl, {
@@ -136,11 +146,19 @@ test.describe('Проверка API пулов для разных пар', () =
       expect(Array.isArray(body.pools)).toBe(true);
       expect(body.pools.length).toBeGreaterThan(0);
 
-      const pool = body.pools.find((pool: Pool) => {
-        return pool.name?.includes(expectedPoolName) || (pool.token0.symbol === expectedToken0Symbol && pool.token1.symbol === expectedToken1Symbol);
+      // const pool = body.pools.find((pool: Pool) => {
+      //   return pool.name?.includes(expectedPoolName) || (pool.token0.symbol === expectedToken0Symbol && pool.token1.symbol === expectedToken1Symbol);
+      // });
+      //
+      // expect(pool).toBeDefined();
+
+      const pool: Pool | undefined = body.pools.find((pool: Pool) => {
+        return pool.poolAddress === expectedPoolAddress;
       });
 
       expect(pool).toBeDefined();
+      expect(pool?.poolAddress).toBe(expectedPoolAddress);
+
     });
   });
 });
